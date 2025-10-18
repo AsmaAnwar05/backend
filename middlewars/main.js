0fdc4ba055b57aeab76b9e8b80f045be9d432155
Asma <asmaanwar1126@gmail.com>
2025-10-18 23:45:04 +0600

    added middlewars folder

---
+++ middlewars/main.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs=require("fs")
+const blog = require('./routes/blog')
+
+// app.use(express.static("public"))
+
+app.use('/blog', blog)
+
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.asma="I am asma";
+    fs.appendFileSync("logs.txt", `${Date.now()} is a ${req.method}\n`, {flag: 'a'})
+  console.log(`${Date.now()} is a ${req.method}`)
+  next()
+})
+app.use((req, res, next) =>{
+  console.log('m2')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+  res.send('Hello about '+req.asma)
+})
+app.get('/contact', (req, res) => {
+  res.send('Hello contact')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
